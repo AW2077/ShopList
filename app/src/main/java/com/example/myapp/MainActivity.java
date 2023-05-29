0173b15e8a4871efe63675586fcdd5c85f7eb174
AW2077 <aga.wlodawiec@gmail.com>
2023-05-30 01:46:30 +0200

    first commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import com.example.myapp.databinding.ActivityMainBinding;
+import com.example.myapp.fragments.FragmentLodowka;
+import com.example.myapp.fragments.FragmentPrzepisy;
+import com.example.myapp.fragments.FragmentZakupy;
+
+import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new FragmentZakupy());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.navigation_zakupy) {
+                replaceFragment(new FragmentZakupy());
+                return true;
+            } else if (itemId == R.id.navigation_lodowka) {
+                replaceFragment(new FragmentLodowka());
+                return true;
+            } else if (itemId == R.id.navigation_przepisy) {
+                replaceFragment(new FragmentPrzepisy());
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayoutId, fragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/example/myapp/fragments/FragmentZakupy.java
@@ -0,0 +1,110 @@
+package com.example.myapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+
+import com.example.myapp.Contact;
+import com.example.myapp.ContactsAdapter;
+import com.example.myapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+public class FragmentZakupy extends Fragment {
+    ArrayList<Contact> contacts;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fr_zakupy, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView rvContacts = view.findViewById(R.id.rvContacts);
+        FloatingActionButton fab = view.findViewById(R.id.floatingActionButton);
+
+        // Initialize contacts
+        contacts = Contact.createContactsList(20);
+        // Create adapter passing in the sample user data
+        ContactsAdapter adapter = new ContactsAdapter(contacts);
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(getContext()));
+        // That's all!
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contacts.add(0, new Contact("new", true));
+                adapter.notifyItemInserted(0);
+                rvContacts.smoothScrollToPosition(0);
+            }
+        });
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                // this method is called
+                // when the item is moved.
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction){
+                // this method is called when we swipe our item to right direction.
+                // on below line we are getting the item at a particular position.
+                Contact deletedCourse = contacts.get(viewHolder.getAdapterPosition());
+
+                // below line is to get the position
+                // of the item at that position.
+                int position = viewHolder.getAdapterPosition();
+
+                // this method is called when item is swiped.
+                // below line is to remove item from our array list.
+                contacts.remove(viewHolder.getAdapterPosition());
+
+                // below line is to notify our item is removed from adapter.
+                adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+
+                // below line is to display our snackbar with action.
+                Snackbar.make(rvContacts, "Usunięto " + deletedCourse.getName(), Snackbar.LENGTH_LONG).setAction("Cofnij", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // adding on click listener to our action of snack bar.
+                        // below line is to add our item to array list with a position.
+                        contacts.add(position, deletedCourse);
+
+                        // below line is to notify item is
+                        // added to our adapter class.
+                        adapter.notifyItemInserted(position);
+                    }
+                }).show();
+            }
+            // at last we are adding this
+            // to our recycler view.
+        }).attachToRecyclerView(rvContacts);
+
+        }
+
+}
